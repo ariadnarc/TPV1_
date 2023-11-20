6273e8a5ad97d5940f6350e00089e27e64a676d2
ariadnarc <ariadr02@ucm.es>
2023-11-20 11:00:19 +0100

    nada util nada util

--- Ariadna/plantillaSDL/Practica1TPV/SceneObject.h
+++ Ariadna/plantillaSDL/Practica1TPV/SceneObject.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include "checkML.h"
+
+
+#include <iostream>
+#include <SDL.h>
+#include <list>
+
+#include "GameObject.h"
+#include "Vector2D.h"
+
+class SceneObject : public GameObject {
+protected:
+
+	Point2D<> pos;
+
+	int width;
+	int height;
+
+
+
+public:
+
+	virtual bool Hit();
+
+};
