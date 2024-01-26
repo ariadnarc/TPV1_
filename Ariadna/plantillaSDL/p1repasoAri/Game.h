1b8ae0fd810ce06ec23513755e43a026e1d86643
ariadnarc <ariadr02@ucm.es>
2024-01-26 12:06:43 +0100

    repasillo ignorar

---
+++ Ariadna/plantillaSDL/p1repasoAri/Game.h
@@ -0,0 +1,12 @@
+#pragma once
+
+using uint = unsigned int;
+
+//tamaño de la ventana
+
+constexpr uint WIND_HEIGHT = 50;
+constexpr uint WIND_WIDTH = 50;
+
+//punteros a la ventana y al renderer
+
+
